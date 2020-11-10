1da1718481b8053198debcce6508f876d314ffad
Strogalev91 <74052206+Strogalev91@users.noreply.github.com>
2020-11-10 20:12:29 +0300

    GoRacing V1.0
    The first version of the first application

---
+++ ResultTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ResultTableViewCell.swift
+//  GoRacing
+//
+//  Created by Strogalev Ilia on 10/11/2020.
+//
+
+import UIKit
+
+class ResultTableViewCell: UITableViewCell {
+    @IBOutlet weak var namePlayer: UILabel!
+    @IBOutlet weak var gameResult: UILabel!
+    @IBOutlet weak var timeResult: UILabel!
+}
