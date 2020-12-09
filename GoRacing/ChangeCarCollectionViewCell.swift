7eaf73d4f12b3c70c80e05464bff44ba0f094cbd
Strogalev Ilia <74052206+Strogalev91@users.noreply.github.com>
2020-12-09 19:38:25 +0300

    Add files via upload

---
+++ GoRacing/ChangeCarCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ChangeCarCollectionViewCell.swift
+//  GoRacing
+//
+//  Created by Strogalev Ilia on 10/11/2020.
+//
+
+import UIKit
+
+class ChangeCarCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var carPicture: UIImageView!
+    
+}
